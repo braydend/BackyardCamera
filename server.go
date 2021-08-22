3e4196ee30870885b3eb53b225b029db07abe1e7
Brayden <braydendiluca@gmail.com>
2021-08-22 18:20:50 +1000

    Create simple HTTP endpoint for taking and uploading photos on demand

--- server.go
+++ server.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"fmt"
+	"gardenPhotosS3/fswebcam"
+	"gardenPhotosS3/s3"
+	"gardenPhotosS3/utils"
+	"net/http"
+	"time"
+)
+
+func handleTakePhoto(w http.ResponseWriter, req *http.Request) {
+	filename := time.Now().Format(time.RFC3339) + ".jpg"
+	timeout, err := time.ParseDuration("5m")
+
+	if err != nil {
+		panic(err)
+	}
+
+	env := utils.GetEnv()
+
+	fswebcam.TakePhoto(filename)
+	s3.UploadPhoto(filename, env.Bucket, env.Region, timeout)
+	utils.DeleteFile(filename)
+	_, err = w.Write([]byte(fmt.Sprintf("Photo %s was taken successfully!", filename)))
+
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
-	$END$
+
+	http.HandleFunc("/takePhoto", handleTakePhoto)
+
+	err := http.ListenAndServe(":8090", nil)
+
+	if err != nil {
+		panic(err)
+	}
 }
