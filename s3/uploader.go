e6121ebacf0cd157a25479570be4a1517662bd0b
Brayden <braydendiluca@gmail.com>
2021-08-22 17:28:07 +1000

    Move S3 fns to own package

---
+++ s3/uploader.go
@@ -0,0 +1,58 @@
+package s3
+
+import (
+	"context"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/request"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"io"
+	"os"
+	"time"
+)
+
+func getS3Client(region string) *s3.S3 {
+	// This might get picked up from .utils
+	az := aws.NewConfig().WithRegion(region)
+	s3Session := session.Must(session.NewSession())
+	return s3.New(s3Session, az)
+}
+
+func Upload(body io.ReadSeeker, bucket, key, region string, timeout time.Duration) {
+	s3Client := getS3Client(region)
+
+	// Create a context with a timeout that will abort the upload if it takes
+	// more than the passed in timeout.
+	ctx := context.Background()
+	var cancelFn func()
+	if timeout > 0 {
+		ctx, cancelFn = context.WithTimeout(ctx, timeout)
+	}
+	// Ensure the context is canceled to prevent leaking.
+	// See context package for more information, https://golang.org/pkg/context/
+	if cancelFn != nil {
+		defer cancelFn()
+	}
+
+	// Uploads the object to S3. The Context will interrupt the request if the
+	// timeout expires.
+	_, err := s3Client.PutObjectWithContext(ctx, &s3.PutObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+		Body:   body,
+	})
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok && aerr.Code() == request.CanceledErrorCode {
+			// If the SDK can determine the request or retry delay was canceled
+			// by a context the CanceledErrorCode error code will be returned.
+			fmt.Fprintf(os.Stderr, "upload canceled due to timeout, %v\n", err)
+		} else {
+			fmt.Fprintf(os.Stderr, "failed to upload object, %v\n", err)
+		}
+		os.Exit(1)
+	}
+
+	fmt.Printf("successfully uploaded file to %s/%s\n", bucket, key)
+}
