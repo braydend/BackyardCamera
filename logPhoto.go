92e7d40546b4c1a09590a0a64c83f35cee5dfda0
Brayden <braydendiluca@gmail.com>
2021-08-22 18:19:41 +1000

    Move more funcs to utils and clean up logPhoto

--- logPhoto.go
+++ logPhoto.go
@@ -5,39 +5,11 @@ import (
 	"gardenPhotosS3/fswebcam"
 	"gardenPhotosS3/s3"
 	"gardenPhotosS3/utils"
-	"os"
 	"time"
 )
 
 type flags struct {
 	timeout time.Duration
-}
-
-type env struct {
-	bucket string
-	region string
-}
-
-func deleteFile(filename string) {
-	err := os.Remove(filename)
-
-	if err != nil {
-		panic(err)
-	}
-}
-
-func takePhoto(filename string) {
-	fswebcam.TakePhoto(filename)
-}
-
-func uploadPhoto(filename, bucket, region string, timeout time.Duration) {
-	file, err := os.Open(filename)
-
-	if err != nil {
-		panic(err)
-	}
-
-	s3.Upload(file, bucket, "testphoto.jpg", region, timeout)
 }
 
 func parseFlags() flags {
@@ -49,21 +21,13 @@ func parseFlags() flags {
 	return flags{timeout}
 }
 
-func getEnv() env {
-	vars := utils.LoadEnv([]string{"BUCKET_NAME", "AWS_REGION"})
-	bucket := vars["BUCKET_NAME"]
-	region := vars["AWS_REGION"]
-
-	return env{bucket, region}
-}
-
 func main() {
 	filename := time.Now().Format(time.RFC3339) + ".jpg"
 
-	env := getEnv()
+	env := utils.GetEnv()
 	flags := parseFlags()
 
-	takePhoto(filename)
-	uploadPhoto(filename, env.bucket, env.region, flags.timeout)
-	deleteFile(filename)
+	fswebcam.TakePhoto(filename)
+	s3.UploadPhoto(filename, env.Bucket, env.Region, flags.timeout)
+	utils.DeleteFile(filename)
 }
